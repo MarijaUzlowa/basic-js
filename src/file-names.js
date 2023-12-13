e95391ef2a90031e071461b2e7f402d227a32e2b
dn071099umi <mariia.uzlova@privatbank.ua>
2023-12-13 19:50:35 +0200

    done 4 + 2 + 2 + 2tasks

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+    // Объект для отслеживания количества встреченных имен файлов
+    const nameCount = {};
+    return names.map(name => {
+        let newName = name;
+        // Проверяем, существует ли имя уже в объекте nameCount
+        if (nameCount[newName] !== undefined) {
+            // Если существует, добавляем суффикс для создания уникального имени
+            while (nameCount[newName] !== undefined) {
+                const suffix = `(${nameCount[newName]})`;
+                // Если имя уже содержит суффикс, заменяем его, иначе добавляем суффикс
+                newName = name.includes('(') ? name.replace(/\(\d+\)$/, suffix) : `${name}${suffix}`;
+                nameCount[newName] = (nameCount[newName] || 0) + 1;
+            }
+        } // Обновляем счетчик для текущего имени
+        nameCount[newName] = (nameCount[newName] || 0) + 1;
+        return newName;
+    });
 }
 
 module.exports = {
-  renameFiles
+    renameFiles
 };
