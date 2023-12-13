071db4baf4874d9b8a6ad4f92be2618350cb385f
dn071099umi <mariia.uzlova@privatbank.ua>
2023-12-13 17:55:12 +0200

    done 4 tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+    // Инициализируем переменную для хранения количества котов
+    let catsCount = 0;
+    // Итерируемся по каждой строке матрицы
+    for (let i = 0; i < matrix.length; i++) {
+        // Итерируемся по каждому элементу в текущей строке
+        for (let j = 0; j < matrix[i].length; j++) {
+            // Проверяем, равен ли текущий элемент '^^'
+            if (matrix[i][j] === '^^') {
+                // Увеличиваем количество котов
+                catsCount++;
+            }
+        }
+    }
+    // Возвращаем окончательное количество котов return catsCount;
+      return catsCount;
 }
 
-module.exports = {
-  countCats
-};
+            module.exports = {
+                countCats
+            };
